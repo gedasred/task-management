7315352ff3f8d57eb60b88f96f4eaf10c5672ea5
Gediminas <118281897+gediminasre@users.noreply.github.com>
2024-12-12 12:08:05 +0200

    n+1 fix, mappers fix, deploy to aws only after ci, reuse default application.properties with env vars

--- src/main/java/com/example/taskmanagement/task/mapper/TaskMapper.java
+++ src/main/java/com/example/taskmanagement/task/mapper/TaskMapper.java
@@ -2,13 +2,32 @@ package com.example.taskmanagement.task.mapper;
 
 import com.example.taskmanagement.task.domain.TaskEntity;
 import com.example.taskmanagement.task.dto.TaskDto;
+import com.example.taskmanagement.user.domain.UserEntity;
+import com.example.taskmanagement.user.dto.UserDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface TaskMapper {
 
+  @Mapping(target = "assignee", qualifiedByName = "mapWithoutAssigneeTasks")
   TaskDto toDto(TaskEntity taskEntity);
 
   TaskEntity toEntity(TaskDto taskDto);
+
+  @Named("mapWithoutAssigneeTasks")
+  default UserDto mapWithoutAssigneeTasks(UserEntity userEntity) {
+    if (userEntity == null) {
+      return null;
+    }
+    return UserDto.builder()
+        .id(userEntity.getId())
+        .firstName(userEntity.getFirstName())
+        .lastName(userEntity.getLastName())
+        .email(userEntity.getEmail())
+        .phoneNumber(userEntity.getPhoneNumber())
+        .build();
+  }
 }
--- src/main/java/com/example/taskmanagement/user/repository/UserRepository.java
+++ src/main/java/com/example/taskmanagement/user/repository/UserRepository.java
@@ -1,6 +1,12 @@
 package com.example.taskmanagement.user.repository;
 
 import com.example.taskmanagement.user.domain.UserEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {}
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+  @Query("SELECT u FROM UserEntity u LEFT JOIN FETCH u.tasks")
+  List<UserEntity> findAllWithTasks();
+}
